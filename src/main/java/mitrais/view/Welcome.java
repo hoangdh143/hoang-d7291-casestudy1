65fb0373a57a577b41566d740d77e95c2235c4a5
HoangDH <huuhoang.dao@gmail.com>
2019-09-16 13:36:50 +0700

    Bugs fixing

--- src/main/java/mitrais/view/Welcome.java
+++ src/main/java/mitrais/view/Welcome.java
@@ -23,37 +23,41 @@ public class Welcome implements View {
 
     @Override
     public void display() {
-        Scanner in = new Scanner(System.in);
-
-        System.out.print("Enter Account Number: ");
-        String accountNumber = in.nextLine();
-
-        System.out.print("Enter PIN: ");
-        String pin = in.nextLine();
-
-        Account account = new Account();
-        account.setAccountNumber(accountNumber);
-        account.setPin(pin);
-
         Set<ValidationStrategy> strategies = new LinkedHashSet<ValidationStrategy>();
         strategies.add(AccountValidationStrategy.ACCOUNT_NUMBER);
         strategies.add(AccountValidationStrategy.PIN);
 
         AccountValidationContext context = new AccountValidationContext(strategies);
+        Account account = new Account();
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter Account Number: ");
+        String accountNumber = in.nextLine();
+        account.setAccountNumber(accountNumber);
+        validate(account, context, this);
+
+        System.out.print("Enter PIN: ");
+        String pin = in.nextLine();
+        account.setPin(pin);
+        validate(account, context, this);
+
+        Account accountDb = accountRepository.get(account.getAccountNumber(), account.getPin());
+
+        if (accountDb == null) {
+            System.out.println("Invalid Account Number/PIN");
+            this.display();
+        }
+
+        dispatcher.setAccount(accountDb);
+        dispatcher.dispatch("TRANSACTION");
+    }
+
+    private void validate(Account account, AccountValidationContext context, View view) {
         String errorCode = context.execute(account);
         if (errorCode != null) {
             System.out.println(errorCode);
-            this.display();
-        } else {
-            Account accountDb = accountRepository.get(account.getAccountNumber(), account.getPin());
-
-            if (accountDb == null) {
-                System.out.println("Invalid Account Number/PIN");
-                this.display();
-            }
-
-            dispatcher.setAccount(accountDb);
-            dispatcher.dispatch("TRANSACTION");
+            view.display();
         }
     }
 }
